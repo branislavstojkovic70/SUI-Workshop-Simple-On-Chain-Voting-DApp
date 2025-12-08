8acf1034e4a5520fd6c61126dfc30f06cf4782f6
Branislav Stojkovic <96660978+branislavstojkovic70@users.noreply.github.com>
2025-12-08 14:32:38 +0100

    hot fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,29 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@mysten/dapp-kit/dist/index.css";
-import "@radix-ui/themes/styles.css";
+import "./index.css";
 
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Theme } from "@radix-ui/themes";
 import App from "./App.tsx";
 import { networkConfig } from "./networkConfig.ts";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./theme.ts";
+import { EnokiFlowProvider } from "@mysten/enoki/react";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-        <QueryClientProvider client={queryClient}>
-          <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
-            <WalletProvider autoConnect>
-              <App />
-            </WalletProvider>
-          </SuiClientProvider>
-        </QueryClientProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider autoConnect>
+          <App />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
